20e453d01655ab282a7297141f35a98ae36aa5c8
xnacly <matteogropp@gmail.com>
2021-05-11 23:46:34 +0200

    added showcase to landingpage

--- frontend/src/components/sites/LandingPage.jsx
+++ frontend/src/components/sites/LandingPage.jsx
@@ -1,5 +1,22 @@
+import Error from "../utils/Error";
+
 function LandingPage() {
-	return <div></div>;
+	return (
+		<>
+			<div style={{ margin: "3rem" }}>
+				<Error type="" text="alert"></Error>
+				<Error type="warning" text="alert.warning"></Error>
+				<Error type="danger" text="alert.danger"></Error>
+				<Error type="success" text="alert.success"></Error>
+			</div>
+			<div style={{ margin: "3rem" }}>
+				<button>button</button>
+				<button className="warning">button.warning</button>
+				<button className="danger">button.danger</button>
+				<button className="success">button.success</button>
+			</div>
+		</>
+	);
 }
 
 export default LandingPage;
