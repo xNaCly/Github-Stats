9f6fb174c578d1cedb7b8aaf09b1e5706d78eacb
xnacly <matteogropp@gmail.com>
2021-05-19 18:34:29 +0200

    increased readability of large numbers

--- frontend/src/components/utils/User.jsx
+++ frontend/src/components/utils/User.jsx
@@ -1,25 +1,41 @@
-function User({ user }) {
+function User({ user, landing = false }) {
 	return (
-		<div className="user_card">
+		<div className={`user_card ${landing && "landingpage_user_card"}`}>
 			<div className="user_card_content">
-				<h3 className="user_card_title">
-					<a href={`https://github.com/${user?.name}`}>{user?.name}</a>
-				</h3>
+				<h5 className="orga_header">
+					<a href={`https://github.com/${user?.name}`}>
+						{user?.name}
+					</a>
+				</h5>
 				<div className="user_card_stat_container">
 					<h5 className="user_card_stat_header">Commits:</h5>
-					<span className="user_card_stat_content">{user?.commits}</span>
+					<span className="user_card_stat_content">
+						{user?.commits?.toLocaleString()}
+					</span>
 				</div>
 				<div className="user_card_stat_container">
 					<h5 className="user_card_stat_header">Additions:</h5>
-					<span className="user_card_stat_content">{user?.additions}</span>
+					<span className="user_card_stat_content">
+						{user?.additions?.toLocaleString()}
+					</span>
 				</div>
 				<div className="user_card_stat_container">
 					<h5 className="user_card_stat_header">Deletions:</h5>
-					<span className="user_card_stat_content">{user?.deletions}</span>
+					<span className="user_card_stat_content">
+						{user?.deletions?.toLocaleString()}
+					</span>
+				</div>
+				<div className="user_card_stat_container">
+					<h5 className="user_card_stat_header">Real additions:</h5>
+					<span className="user_card_stat_content">
+						{user?.realAdditions?.toLocaleString()}
+					</span>
 				</div>
 				<div className="user_card_stat_container">
 					<h5 className="user_card_stat_header">Addtions/Commit:</h5>
-					<span className="user_card_stat_content">{user?.additionsPerCommit}</span>
+					<span className="user_card_stat_content">
+						{user?.additionsPerCommit?.toLocaleString()}
+					</span>
 				</div>
 			</div>
 		</div>
